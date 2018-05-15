5cd4e936763a450ae5be6f545b7cbcc3af150f1a
rajin4884 <rajin4884@naver.com>
2018-05-15 15:50:28 +0900

    starters

--- team/models.py
+++ team/models.py
@@ -122,9 +122,9 @@ class Board(models.Model):
 
 # 후기 게시판
 class Review(models.Model):
-    mrapply_Num = models.ForeignKey('Mrapply', on_delete=models.CASCADE)
+    mrapply_num = models.ForeignKey('Mrapply', on_delete=models.CASCADE)
     r_num = models.IntegerField(primary_key=True)
-    mrapply_Num = models.IntegerField(null = True, blank = True)
+    mrapply_num = models.IntegerField(null = True, blank = True)
     mr_title = models.CharField("제목", max_length = 20, null = True, blank = False)
     mto_name = models.CharField("멘토이름", max_length = 20, null = True, blank = False)
     r_score = models.IntegerField("조회수", null = True, blank = True, default=0) # 조회 수
